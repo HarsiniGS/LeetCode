0d138badad91bfdc87caa6492d5aac2aac6753c7
HARSINI GS <harsinigs@gmail.com>
2026-03-26 11:32:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,41 +1,42 @@
-import java.util.*;
-
 class Solution {
-    List<List<String>> res = new ArrayList<>();
+    public List<List<String>> res=new ArrayList<>();
     public List<List<String>> solveNQueens(int n) {
-        char[][] board = new char[n][n];
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(board[i], '.');
+        char board[][]=new char[n][n];
+        for(int i=0;i<n;i++){
+            Arrays.fill(board[i],'.');
         }
-        backtrack(board, 0);
+        backtrack(board,0);
         return res;
     }
-    public void backtrack(char[][] board, int row) {
-        if (row == board.length) {
-            List<String> list = new ArrayList<>();
-            for (int i = 0; i < board.length; i++) {
-                list.add(new String(board[i]));
+    public void backtrack(char board[][],int row){
+        if(row==board.length){
+            ArrayList<String> l=new ArrayList<>();
+            for(int i=0;i<board.length;i++){
+                l.add(new String(board[i]));
             }
-            res.add(list);
+            res.add(l);
             return;
         }
-        for (int col = 0; col < board.length; col++) {
-            if (isSafe(board, row, col)) {
-                board[row][col] = 'Q';
-                backtrack(board, row + 1);
-                board[row][col] = '.';
+        for(int col=0;col<board[row].length;col++){
+            if(isSafe(board,row,col)){
+                board[row][col]='Q';
+                backtrack(board,row+1);
+                board[row][col]='.';
             }
         }
     }
-    public boolean isSafe(char[][] board, int row, int col) {
-        for (int i = 0; i < row; i++) {
-            if (board[i][col] == 'Q') return false;
-        }
-        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j] == 'Q') return false;
-        }
-        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
-            if (board[i][j] == 'Q') return false;
+    public boolean isSafe(char board[][], int row, int col){
+        for(int i=0;i<row;i++){
+            if(board[i][col]=='Q'){
+                return false;
+            }
+            for(int j=0;j<board[row].length;j++){
+                if(board[i][j]=='Q'){
+                    if(Math.abs(row-i)==Math.abs(col-j)){
+                        return false;
+                    }
+                }
+            }
         }
         return true;
     }
