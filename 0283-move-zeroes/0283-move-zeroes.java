937835860a2002a7856328fdc7cda0ac0f326cdb
HARSINI GS <harsinigs@gmail.com>
2026-04-05 10:56:09 +0530

    Time: 2 ms (91.01%), Space: 47.4 MB (94.5%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n=nums.length;
+        int []arr=new int[n];
+
+        int cnt = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i] != 0) arr[cnt++] = nums[i];
+        }
+        for(int i=0;i<n;i++){
+            nums[i] = arr[i];
+        }
+    }
+}
